b2401e2cf38aed33d13e49330f2d11de14b36da6
Opus1no2 <tillotson.travis@gmail.com>
2020-12-19 18:14:43 -0600

    day 2 pt 2

--- 2020/3/Algo.mjs
+++ 2020/3/Algo.mjs
@@ -4,6 +4,7 @@ class Algo {
   }
 
   part1() {
+    let count = 0;
     const xFactor = 3
     const y = this.input.length - 1;
     const x = y * xFactor;
@@ -22,10 +23,7 @@ class Algo {
       matrix.push(str);
     }
 
-    let count = 0;
-
     for (let i = 0; i < matrix.length; i++) {
-
       if (matrix[i][i * xFactor] === "#") {
         count += 1;
       }
@@ -34,7 +32,33 @@ class Algo {
     return count;
   }
 
-  part2() {}
+  part2(xFactor, yFactor) {
+    let count = 0;
+    const y = this.input.length - 1;
+    const x = y * xFactor;
+
+    const cols = Math.ceil(x / (this.input[0].length - 1));
+
+    let matrix = [];
+
+    for (let i = 0; i < this.input.length; i++) {
+      let str = '';
+
+      for (let j = 0; j < cols + 1; j++) {
+        str += this.input[i];
+      }
+
+      matrix.push(str);
+    }
+
+    for (let i = yFactor, j = xFactor; i < matrix.length; i += yFactor, j += xFactor) {
+      if (matrix[i][j] === "#") {
+        count += 1;
+      }
+    }
+
+    return count;
+  }
 }
 
 export default Algo;
--- 2020/3/test.mjs
+++ 2020/3/test.mjs
@@ -4,17 +4,41 @@ import test from '../../test.mjs';
 
 const rawTestInput = fs.readFileSync('./test_input.txt', 'utf-8');
 const testInput = rawTestInput.split("\n").filter(n => n);
-
 const testAlgo = new Algo(testInput);
-const answer = 8;
-//const answer2 = 2;
-
-test(answer, testAlgo.part1());
-//test(answer2, testAlgo.part2());
 
 const input = fs.readFileSync('./input.txt', 'utf-8').split("\n").filter(n => n);
 const algo = new Algo(input);
 
+test(2, testAlgo.part2(1, 1));
+test(7, testAlgo.part2(3, 1));
+test(3, testAlgo.part2(5, 1));
+test(4, testAlgo.part2(7, 1));
+test(2, testAlgo.part2(1, 2));
 console.log("answer for part 1:", algo.part1());
-//console.log("answer for part 2:", algo.part2());
 
+const args = [
+  [1, 1],
+  [3, 1],
+  [5, 1],
+  [7, 1],
+  [1, 2],
+];
+
+let answers = [];
+let testAnswers = [];
+
+for (let i = 0; i < args.length; i++) {
+  answers.push(algo.part2(...args[i]));
+  testAnswers.push(testAlgo.part2(...args[i]));
+}
+
+let testAnswer = testAnswers.reduce((pre, curr) => {
+  return pre * curr;
+});
+
+let answer = answers.reduce((pre, curr) => {
+  return pre * curr;
+});
+
+console.log("test answer for part 2:", testAnswer);
+console.log("answer for part 2:", answer);
