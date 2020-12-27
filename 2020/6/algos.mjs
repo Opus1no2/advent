8c4b50a08f8de10d78c987bf18aaa95bc2dbf045
Opus1no2 <tillotson.travis@gmail.com>
2020-12-27 11:07:50 -0600

    day 6 part 2

--- 2020/6/algos.mjs
+++ 2020/6/algos.mjs
@@ -5,7 +5,33 @@ const part1 = (input) => {
 };
 
 const part2 = (input) => {
+  let count = 0;
+  const onePersons = input.filter(r => r.length === 1);
+  const couples = input.filter(r => r.length > 1);
 
+  count += onePersons.reduce((pre, curr) => {
+    return pre + [...new Set(...curr)].length;
+  }, 0);
+
+  couples.forEach((row) => {
+    let hash = [...row.join('')].reduce((pre, curr) => {
+      if (!pre[curr]) {
+        pre[curr] = 1;
+      } else {
+        pre[curr] += 1;
+      }
+
+      return pre;
+    }, {});
+
+    for (let key in hash) {
+      if(hash[key] === row.length) {
+        count += 1;
+      }
+    }
+  });
+
+  return count;
 };
 
 export { part1, part2 };
--- 2020/6/test.mjs
+++ 2020/6/test.mjs
@@ -7,11 +7,25 @@ const input = fs.readFileSync('./input.txt', 'utf-8')
 	.filter(n => n)
 	.map(v => v.replace(/\n/g, ''));
 
+const input2 = fs.readFileSync('./input.txt', 'utf-8')
+	.split("\n\n")
+	.filter(n => n)
+	.map(v => v.split(/\n/g))
+  .filter(n => n);
+
 const testInput = fs.readFileSync('./test_input.txt', 'utf-8')
 	.split("\n\n")
 	.filter(n => n)
 	.map(v => v.replace(/\n/g, ''));
 
+const testInput2 = fs.readFileSync('./test_input.txt', 'utf-8')
+	.split("\n\n")
+	.filter(n => n)
+	.map(v => v.split(/\n/g)
+	.filter(n => n));
+
 test(11, part1(testInput));
+test(6, part2(testInput2));
 
 console.log("answer for part 1:", part1(input));
+console.log("answer for part 2:", part2(input2));
