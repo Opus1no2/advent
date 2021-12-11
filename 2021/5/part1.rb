1ce9c20e15d6829892c60c5e97ffcd54fe0e0dee
Opus1no2 <tillotson.travis@gmail.com>
2021-12-10 22:02:29 -0600

    clean up day 5

--- 2021/5/part1.rb
+++ 2021/5/part1.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 input = File.read('input.txt').split("\n")
 
 max_x = -1
@@ -5,7 +7,7 @@ max_y = -1
 xy = []
 
 input.each do |str|
-  coords = str.split(" -> ").map { |i| i.split(",")}
+  coords = str.split(' -> ').map { |i| i.split(',') }
 
   x1, x2 = coords.map(&:first).map(&:to_i).sort
   y1, y2 = coords.map(&:last).map(&:to_i).sort
@@ -16,9 +18,7 @@ input.each do |str|
   max_x = mx > max_x ? mx : max_x
   max_y = my > max_y ? my : max_y
 
-  if x1 == x2 || y1 == y2
-    xy << { x1: x1.to_i, x2: x2.to_i, y1: y1.to_i, y2: y2.to_i }
-  end
+  xy << { x1: x1, x2: x2, y1: y1, y2: y2 } if x1 == x2 || y1 == y2
 end
 
 grid = Array.new(max_y + 1) { Array.new(max_x + 1, 0) }
@@ -31,17 +31,17 @@ xy.each do |coords|
     end
   end
 
-  if coords[:x1] == coords[:x2]
-    indexes = (coords[:y1]..coords[:y2]).to_a
-    indexes.each do |idx|
-      grid[idx][coords[:x1]] += 1
-    end
+  next unless coords[:x1] == coords[:x2]
+
+  indexes = (coords[:y1]..coords[:y2]).to_a
+  indexes.each do |idx|
+    grid[idx][coords[:x1]] += 1
   end
 end
 
-sum = grid.flatten.inject(0) do |sum, val|
+answer = grid.flatten.inject(0) do |sum, val|
   sum += 1 if val > 1
   sum
 end
 
-p sum
+p answer
